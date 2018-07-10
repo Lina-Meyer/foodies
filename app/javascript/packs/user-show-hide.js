e034338c6bb69d859fd32db7cecb90e622ba85d2
Simon Sohm <Simon.Sohm@carini.at>
2018-07-10 13:54:22 +0200

    design fixes on home page

--- app/javascript/packs/user-show-hide.js
+++ app/javascript/packs/user-show-hide.js
@@ -2,26 +2,19 @@ $('.reviews-all-user').click(function() {
     $('.user-reviews').toggle('slow');
     $('.top-5-user').hide(0);
     $('.wishlist-user').hide(0);
-    $('.Freundschaftsanfragen-list').hide(0);
-
+    $('.freundschaftsanfragen-list').hide(0);
 });
 
-$('.top-5').click(function() {
-    $('.top-5-user').toggle('slow');
-    $('.user-reviews').hide(0);
-    $('.wishlist-user').hide(0);
-    $('.Freundschaftsanfragen-list').hide(0);
-});
 
 $('.wishlist').click(function() {
     $('.wishlist-user').toggle('slow');
     $('.user-reviews').hide(0);
     $('.top-5-user').hide(0);
-    $('.Freundschaftsanfragen-list').hide(0);
+    $('.freundschaftsanfragen-list').hide(0);
 });
 
 $('.Freundschaftsanfragen').click(function(){
-    $('.Freundschaftsanfragen-list').toggle('slow');
+    $('.freundschaftsanfragen-list').toggle('slow');
     $('.top-5-user').hide(0);
     $('.wishlist-user').hide(0);
     $('.user-reviews').hide(0);
