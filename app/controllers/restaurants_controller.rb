adafc3386a44563ac9eb33d3f080ccf3232aa5df
Lina Meyer <linammeyer@gmail.com>
2018-07-21 12:57:26 +0200

    jetzt werden zwei ratings vom gleichen restaurant in einme zusammen gefasst

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -34,11 +34,68 @@ class RestaurantsController < ApplicationController
       end
     end
 
+    # @unique = @restaurants_pure.uniq{ |d| d[:name]}
+
+# find all names of the restaurants to see which names are double
+    @arr = []
+    @restaurants_pure.each do |restaurant|
+      @arr << restaurant.name
+    end
+
+# find all double names
+    @doubles = @arr.find_all { |e| @arr.count(e) > 1 }.uniq
+
+#find all restaurants that are not listed more than once
+    @restaurants_ohne_doubles = []
+
+    @restaurants_pure.each do |restaurant|
+      if @doubles.exclude?(restaurant.name) == true
+        @restaurants_ohne_doubles << restaurant
+      end
+    end
+
+# list all restaurants that are duplicates
+    @restaurants_doubles = []
+    @restaurants_pure.each do |restaurant|
+      if @doubles.exclude?(restaurant.name) == false
+        @restaurants_doubles << restaurant
+      end
+    end
+
 
     if params[:city].present?
 
       # @restaurants = @restaurants_pure(city: params[:city])
-      @restaurants = @restaurants_pure.select do |restaurant|
+      @restaurants_ohne = @restaurants_ohne_doubles.select do |restaurant|
+        restaurant.city == params[:city]
+      end
+      # @restaurants = @restaurants_list.where(city: params[:city])
+    else
+      @restaurants_ohne = []
+    end
+
+    if params[:city].present?
+
+      # @restaurants = @restaurants_pure(city: params[:city])
+      @restaurants_mit = @restaurants_doubles.select do |restaurant|
+        restaurant.city == params[:city]
+      end
+      # @restaurants = @restaurants_list.where(city: params[:city])
+    else
+      @restaurants_mit = []
+    end
+
+    @restaurant_first = @restaurants_mit.first
+
+    @all_users = []
+    @restaurants_mit.each do |restaurant|
+      @all_users << restaurant.users
+    end
+
+    if params[:city].present?
+
+      # @restaurants = @restaurants_pure(city: params[:city])
+      @restaurants = @restaurants_doubles.select do |restaurant|
         restaurant.city == params[:city]
       end
       # @restaurants = @restaurants_list.where(city: params[:city])
