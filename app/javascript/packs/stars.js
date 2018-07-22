3e7f2c812cb19a213ca77f206661733e463fbe4b
Lina Meyer <linammeyer@gmail.com>
2018-07-22 23:55:07 +0200

    stars in heroku

--- app/javascript/packs/stars.js
+++ app/javascript/packs/stars.js
@@ -3,6 +3,7 @@ import "jquery-bar-rating";
 
 $(function() {
   $('#review_stars').barrating({theme: 'fontawesome-stars'});
+  console.log('#review_stars')
   $('.review-stars-select').each(function() {
     $(this).barrating({theme: 'fontawesome-stars', readonly: true, emptyValue: 0});
   })
